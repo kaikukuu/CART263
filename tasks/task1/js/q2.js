2ca20a73f86981640d2bd6e6d8401e1e8fe39003
allecatt <157419169+kaikukuu@users.noreply.github.com>
2026-01-15 14:19:25 -0500

    task2

--- tasks/task1/js/q2.js
+++ tasks/task1/js/q2.js
@@ -6,9 +6,12 @@
 
 function setup() {
     console.log("go")
-
+    drawEllipse(50, 50, 80, 40, 255, 0, 0);
+    drawEllipse(200, 100, 60, 60, 0, 255, 0);
+    drawEllipse(350, 150, 100, 50, 0, 0, 255);
 }
 
-function draw() {
-
+function drawEllipse(x, y, w, h, r, g, b) {
+    fill(r, g, b);
+    ellipse(x, y, w, h);
 }
