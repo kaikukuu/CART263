d33b8b519848831941242e746273f96072a8dcd5
allecatt <157419169+kaikukuu@users.noreply.github.com>
2026-01-29 14:38:11 -0500

    event listener in class

--- tasks/week3/js/3_events.js
+++ tasks/week3/js/3_events.js
@@ -2,12 +2,36 @@ window.onload = setup;
 function setup() {
     console.log("events!")
 
-    let introSection = document.querySelector("#intro");
-    introSection.addEventListener("click", function (e) {
-        console.log(this);
-        console.log(e)
+    let section = document.querySelectorAll(".mouseclick-active-section");
+    section.forEach(element => {
+        element.addEventListener("click", function (e) {
+            console.log(this);
+            console.log(e)
+            this.style.opacity = 0.5;
+        });
 
-        //a:
-        this.style.background = `rgba(214, 110, 239, 0.5)`
     });
+
+
+    // let practiceSection = document.querySelector("#s1");
+    // practiceSection.addEventListener("mousemove",
+    //     function (e) {
+    //         console.log(this);
+    //         console.log(e)
+
+    //         //a:
+    //         this.style.background = `rgba(222, 111, 333, 0.5)`
+    //     }
+    // );
+
+    // let practiceSection2 = document.querySelector("#s2");
+    // practiceSection2 .addEventListener("keydown",
+    //     function (e) {
+    //         console.log(this);
+    //         console.log(e)
+
+    //         //a:
+    //         this.style.background = `rgba(222, 111, 333, 0.5)`
+    //     }
+    // );
 }
