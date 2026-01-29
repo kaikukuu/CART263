4f6cc916fe77520188d46848e5cac8803fb9f22d
allecatt <157419169+kaikukuu@users.noreply.github.com>
2026-01-29 15:02:29 -0500

    testing in setup() js

--- tasks/week3/js/3_events.js
+++ tasks/week3/js/3_events.js
@@ -7,9 +7,40 @@ function setup() {
         element.addEventListener("click", function (e) {
             console.log(this);
             console.log(e)
-            this.style.opacity = 0.5;
+
+            //check if is inactive
+            if (this.getAttribute("custom-bool") === "inactive") {
+                console.log("is inactive");
+                console.log(this.id)
+                this.setAttribute("custom-bool", "active");
+                this.style.opacity = 0.5;
+            } else {
+                console.log("is active");
+                console.log(this.id)
+                this.setAttribute("custom-bool", "inactive");
+                this.style.opacity = 1.5;
+            }
+
+
         });
 
+        document.querySelector("#bubbleButton").addEventListener("click", function (e) {
+            console.log("Button clicked!");
+            let bubble = document.createElement("div");
+            bubble.classList.add("bubble");
+            bubble.style.left = `${Math.random() * (window.innerWidth - 200)}px`;
+            console.log(bubble.style.left);
+
+            bubble.style.top = `${Math.random() * (window.innerHeight - 200)}px`;
+
+            let r = Math.ceil(Math.random() * 255); //new Math.ceil
+            let g = Math.ceil(Math.random() * 255);
+            let b = Math.ceil(Math.random() * 255);
+
+            bubble.style.background = `rgba(${r},${g},${b})`;
+            document.getElementById("top-layer").appendChild(bubble)
+
+        });
     });
 
 
