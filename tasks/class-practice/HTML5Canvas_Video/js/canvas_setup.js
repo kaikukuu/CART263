b6c778e8d5505826b36ea159c1e2023e3a5d258a
allecatt <157419169+kaikukuu@users.noreply.github.com>
2026-02-26 14:14:09 -0500

    VII : CANVAS & MEDIA API's

---
+++ tasks/class-practice/HTML5Canvas_Video/js/canvas_setup.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+    // get the canvas
+    let canvas = document.getElementById("testCanvas");
+    //get the context
+    let context = canvas.getContext("2d");
+
+    let rectSize = 50
+    //a draw a rect:
+    context.fillStyle = "rgba(255,0,0,255)";
+    // draw a rect
+    context.fillRect(canvas.width / 2, canvas.height / 2, rectSize, rectSize);
+    // cut out a rect inside
+    context.clearRect(canvas.width / 2 + rectSize / 4, canvas.height / 2 + rectSize / 4, rectSize / 2, rectSize / 2);
+
+    let xPos = canvas.width / 3;
+    let yPos = canvas.height / 2;
+    let radius = 40;
+    let startAngle = 0;
+    let endAngle = Math.PI * 2; //full rotation
+
+    context.beginPath();
+    context.arc(xPos, yPos, radius, startAngle, endAngle, true);
+    context.fill(); // set the fill
+    context.lineWidth = 2; //change stroke
+    context.stroke();//set the stroke
+    context.closePath();
+
+    //SECOND
+    context.strokeStyle = "#0000FF"; // change the color we are using
+    context.beginPath();
+    context.arc(xPos + 200, yPos, radius, startAngle, endAngle, true);
+    context.fill(); // set the fill
+    context.lineWidth = 2; //change stroke
+    context.stroke();//set the stroke
+    context.closePath();
+
+};
